d58757b0795a101a335481ca28ccd7cb289fec84
GFigs <sgabrielfigueroa95@gmail.com>
2025-04-06 14:44:19 +0200

    add CashRegister page route

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CashRegisterPage from '@/pages/CashRegisterPage';
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<CashRegisterPage />} />
+                <Route path="/cash-register" element={<CashRegisterPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes
