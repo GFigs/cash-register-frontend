bd2260fc51fdc5b1764a7e4acb3d74ebe1063cc9
GFigs <sgabrielfigueroa95@gmail.com>
2025-04-07 01:24:24 +0200

    fix: correct axios config and cash-register page

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http//localhost:3000",
+    baseURL: "http://localhost:3000",
     headers: {
         "Content-Type": "application/json",
-        Accept: "application/jason",
+        Accept: "application/json",
     }
 })
 
